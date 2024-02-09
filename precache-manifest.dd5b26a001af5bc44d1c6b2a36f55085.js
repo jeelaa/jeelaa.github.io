34fd5c480f13cb3bf7c1ff80ad999db1161ba136
Jeela <intouchemrweb@gmail.com>
2024-02-09 17:47:08 -0500

    second commit

---
+++ precache-manifest.dd5b26a001af5bc44d1c6b2a36f55085.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "582e837dc451cf65e17c",
+    "url": "/static/css/main.b0fa7a99.chunk.css"
+  },
+  {
+    "revision": "582e837dc451cf65e17c",
+    "url": "/static/js/main.582e837d.chunk.js"
+  },
+  {
+    "revision": "75aa2d861595ad62f4c5",
+    "url": "/static/js/1.75aa2d86.chunk.js"
+  },
+  {
+    "revision": "c42c66694d4a0b3c2431",
+    "url": "/static/css/2.3e84e3e1.chunk.css"
+  },
+  {
+    "revision": "c42c66694d4a0b3c2431",
+    "url": "/static/js/2.c42c6669.chunk.js"
+  },
+  {
+    "revision": "87f1668ef9aa68ad008a",
+    "url": "/static/js/runtime~main.87f1668e.js"
+  },
+  {
+    "revision": "c186c8111aa9aff67636369d9149c52a",
+    "url": "/static/media/cn-gif-1.c186c811.gif"
+  },
+  {
+    "revision": "fde286a664d36cc9ea93a11d077db4c9",
+    "url": "/static/media/chiropractic-newsletter-template-med.fde286a6.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6e20876691d28375d7540f0c8fb5f8f1",
+    "url": "/index.html"
+  }
+];
